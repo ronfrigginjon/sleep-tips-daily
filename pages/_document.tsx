34a68b7956a34c6d53f7efcbeb6243ac7b47745b
ronfrigginjon <ryanfinley7@gmail.com>
2025-08-26 16:58:24 -0500

    takeads verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+	    <meta name="takeads-platform-verification" content="3abead6b-064d-4bbb-a9d9-7c550bd140a2"/>
         <meta name="theme-color" content="#ffffff" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <meta name="robots" content="index,follow" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,11 @@ import Head from "next/head";
 export default function Home() {
   return (<>
     <Head>
-      <meta name="takeads-platform-verification" content="3abead6b-064d-4bbb-a9d9-7c550bd140a2"/>
-	  <title>Sleep Tips Daily — Nightly, 30-Second Sleep Advice</title>
+      <title>Sleep Tips Daily — Nightly, 30-Second Sleep Advice</title>
       <meta
         name="description"
         content="Join Sleep Tips Daily for a 30-second, science-backed sleep tip every evening. Build a better bedtime routine and sleep deeper."
       />
-	  
       <meta name="robots" content="index, follow" />
       <link rel="canonical" href="https://sleeptipsdaily.com" />
       
